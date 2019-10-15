347b837fa1b310b4572bcce678536ca6b9c95826
Osama Elhussiny <osama.elhussiny@esrinea.local>
2019-10-15 17:02:38 +0200

    added dropmenu nuget

--- EsriNeaMvvmPattern/EsriNeaMvvmPattern/ViewModels/Home/MainViewModel.cs
+++ EsriNeaMvvmPattern/EsriNeaMvvmPattern/ViewModels/Home/MainViewModel.cs
@@ -1,20 +1,64 @@
-﻿using EsriNeaMvvm;
+﻿using DropdownMenu.Models.DropdownModel;
+using EsriNeaMvvm;
 using FreshMvvmPattern.ViewModels.Login;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace FreshMvvmPattern.ViewModels.Home
 {
     public class MainViewModel : BaseViewModel
     {
+        public DropMenuModel GenderList { get; set; }
+        public DropMenuModel LookingforList { get; set; }
+        public ICommand GenderSelectedCommand { get; set; }
         public AwaitCommand GoBackCommand { get; }
         public MainViewModel()
         {
-            GoBackCommand = new AwaitCommand(GoBackCommandExecute);
-        }
+            GenderList = new DropMenuModel
+            {
+                Id = 1,
+                HeaderName = "Gender",
+                DropMenuList = new ObservableCollection<DropMenuItemModel>
+                    {
+                        new DropMenuItemModel
+                        {
+                            Id = 1,
+                            Name = "Male",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 2,
+                            Name = "Female",
+                            IsSelected = false,
+                        },
+                        new DropMenuItemModel
+                        {
+                            Id = 3,
+                            Name = "Other",
+                            IsSelected = false,
+                        },
+                    }
+            };
 
+            GoBackCommand = new AwaitCommand(GoBackCommandExecute);
+            GenderSelectedCommand = new Command(GenderSelectedCommandExecute);
+
+        }
+        private void GenderSelectedCommandExecute(object obj)
+        {
+            //first we set all is selected == false then we set new value
+            GenderList.DropMenuList.Select(x => { x.IsSelected = false; return x; }).ToList();
+
+            if (obj is DropMenuItemModel selecteditem)
+                GenderList.DropMenuList.FirstOrDefault(x => x.Id == selecteditem.Id).IsSelected = true;
+        }
         private async void GoBackCommandExecute(object arg1, TaskCompletionSource<bool> arg2)
         {
              await NavigationService.PopPageModel("adada"); 
